0dea4873f1150c3d29e466ea0088e6a1e2702101
Kazoroo <kazoroo1903@gmail.com>
2025-04-09 18:57:31 +0200

    Add open/close animation for accordion.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {NgOptimizedImage} from '@angular/common';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NgOptimizedImage],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  animations: [
+    trigger('openClose', [
+      state(
+        'open',
+        style({
+          maxHeight: '150px',
+        })
+      ),
+      state(
+        'closed',
+        style({
+          maxHeight: '0px',
+          padding: '0px',
+          // borderTop: '0px'
+          borderWidth: '0px'
+        })
+      ),
+      transition('open => closed', [animate('0.3s ease-in-out')]),
+      transition('closed => open', [animate('0.3s ease-in-out')]),
+    ])
+  ]
 })
 export class AppComponent {
   isAccordionActive = true
