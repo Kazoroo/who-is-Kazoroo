e5e30ff8e7ec1ad20e0ab8b62095467641538451
Kazoroo <kazoroo1903@gmail.com>
2025-04-16 20:31:50 +0200

    Add content for 'Project' accordion in Projects subsite and properly align it. Add onHover gradient effect for accordion items.

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 import {RouterLink, RouterOutlet} from "@angular/router";
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-projects',
@@ -10,7 +11,27 @@ import {RouterLink, RouterOutlet} from "@angular/router";
     RouterLink
   ],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.css'
+  styleUrl: './projects.component.css',
+  animations: [
+    trigger('openClose', [
+      state(
+        'open',
+        style({
+          maxHeight: '150px',
+        })
+      ),
+      state(
+        'closed',
+        style({
+          maxHeight: '0px',
+          padding: '0px',
+          borderWidth: '0px'
+        })
+      ),
+      transition('open => closed', [animate('0.3s ease-in-out')]),
+      transition('closed => open', [animate('0.3s ease-in-out')]),
+    ])
+  ]
 })
 export class ProjectsComponent {
   isAccordionActive = true
